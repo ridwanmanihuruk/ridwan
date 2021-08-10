89e5ea0c1a76582b740493bc01af135276d237bc
Ridwan <ridwanmnhuruk@gmail.com>
2021-08-10 14:06:27 +0700

    ridwan

--- mywebsite/mywebsite/settings.py
+++ mywebsite/mywebsite/settings.py
@@ -15,10 +15,11 @@ import os
 import dj_database_url
 
 
+
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 #BASE_DIR = Path(__file__).resolve().parent.parent
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
-
+PRODUCTION = os.environ.get('DATABASE_URL') is not None
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
